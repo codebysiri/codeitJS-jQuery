6519998f94583dc0ecac483c9e3ed3fa3ff344a5
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-02-14 23:40:49 +0900

    Create project

--- JS/Chapter02/Ex0319/main.js
+++ JS/Chapter02/Ex0319/main.js
@@ -1,9 +1,9 @@
 let current = 1;
 let previous = 0;
 
-for (let i = 1; i <= 50; i++) {
+for (let i = 1; i <= 10; i++) {
     console.log(current);
-    let tmp = previous;
+    let tmp = previous
     previous = current;
     current = current + tmp;
 }
