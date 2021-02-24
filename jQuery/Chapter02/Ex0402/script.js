4023d9ec2c16df34cc0ee00f2eed8a796267a7a9
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-02-24 22:38:09 +0900

    Create project

---
+++ jQuery/Chapter02/Ex0402/script.js
@@ -0,0 +1,20 @@
+function scrollHandler() {
+    if ($(window).scrollTop() >= $('.about').position().top) {
+        $('.menu-right button').css('color', '#4a4a4a');
+    } else {
+        $('.menu-right button').css('color', 'white')
+    }
+}
+
+$(window).on('scroll', scrollHandler);
+
+scrollHandler();
+
+$('.menu-right button').on('click', function() {
+   var id = $(this).attr('id');
+   if (id == "about-btn") {
+       $('html, body').animate({scrollTop: $('.about').position().top}, 1000);
+   } else if (id == "contact-btn") {
+       $('html, body').animate({scrollTop: $('.contact').position().top}, 1000);
+   }
+});
