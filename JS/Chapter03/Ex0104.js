7b6a3970e9b91234c96f67bb5017663a1e9652d0
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-09-16 23:37:32 +0900

    update project

--- JS/Chapter03/Ex0104.js
+++ JS/Chapter03/Ex0104.js
@@ -1,10 +1,11 @@
 let myVoca = {
-    function: '함수',
-    variable: '변수',
-    constant: '상수',
-    local: '지역의',
-    global: '전반적인'
-}
+	// 코드를 작성해 주세요.
+    function : '함수',
+    variable : '변수',
+    constant : '상수',
+    local : '지역의',
+    global : '전반적인'
+};
 
 console.log(myVoca);
 console.log(myVoca.local);
--- JS/Chapter03/Ex0108.js
+++ JS/Chapter03/Ex0108.js
@@ -1,29 +1,29 @@
 let myVoca = {
-
-    addVoca: function (word, mean) {
+    // 코드를 작성해 주세요.
+    addVoca : function(word, mean) {
         myVoca[word] = mean;
     },
 
-    deleteVoca: function (word) {
+    deleteVoca : function(word) {
         delete myVoca[word];
     },
 
-    printVoca: function (word) {
-        console.log(`"${word}"의 뜻은 "${myVoca[word]}"입니다.`)
+    printVoca : function(word) {
+        console.log(`"${word}"의 뜻은 "${myVoca[word]}"입니다.`);
     }
-
-};
-
-// addVoca메소드 테스트 코드
-myVoca.addVoca('parameter', '매개 변수');
-myVoca.addVoca('element', '요소');
-myVoca.addVoca('property', '속성');
-console.log(myVoca);
-
-// deleteVoca메소드 테스트 코드
-myVoca.deleteVoca('parameter');
-myVoca.deleteVoca('element');
-console.log(myVoca);
-
-// printVoca메소드 테스트 코드
-myVoca.printVoca('property');
+  
+  };
+  
+  // addVoca메소드 테스트 코드
+  myVoca.addVoca('parameter', '매개 변수');
+  myVoca.addVoca('element', '요소');
+  myVoca.addVoca('property', '속성');
+  console.log(myVoca);
+  
+  // deleteVoca메소드 테스트 코드
+  myVoca.deleteVoca('parameter');
+  myVoca.deleteVoca('element');
+  console.log(myVoca);
+  
+  // printVoca메소드 테스트 코드
+  myVoca.printVoca('property');
