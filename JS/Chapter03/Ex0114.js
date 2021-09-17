d5d0d194f085f43bea247e19434ef854f50dbc5b
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-09-17 23:05:53 +0900

    update project

--- JS/Chapter03/Ex0114.js
+++ JS/Chapter03/Ex0114.js
@@ -2,8 +2,9 @@ let today = new Date(2112, 8, 24);
 let jaeSangStart = new Date(2109, 7, 1);
 
 function workDayCalc(startDate) {
+	// 여기에 코드를 작성해 주세요.
     let calculate = (Number(today)/1000/60/60/24 - Number(jaeSangStart)/1000/60/60/24) + 1;
-    console.log(`오늘은 입사한 지 ${calculate}일째 되는 날 입니다.`);
+	console.log(`오늘은 입사한 지 ${calculate}일째 되는 날 입니다.`)
 }
 
 workDayCalc(jaeSangStart);
